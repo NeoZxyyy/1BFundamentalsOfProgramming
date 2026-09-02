0d2637f20aea222f3856af8bcb283d7e1a93331e
Neo <oabal34@gmail.com>
2026-09-02 20:22:57 +0800

    Assignment_3

---
+++ src/Assignment_3.java
@@ -0,0 +1,20 @@
+public class Assignment_3 { // Defines where the program will run //
+
+    public static void main(String[] args) { // Where Java starts executing the program //
+
+        String firstName = "Oliver Cromwell"; // Stores the user's first name as a String //
+        String lastName = "Abalos"; // Stores the user's last name as a String //
+        String address = "Laoac"; // Stores the user's address as a String //
+        String mobileNumber = "09297923375"; // Stores the mobile number as a String to preserve the leading zero //
+        int age = 20; // Stores the user's age as an integer //
+        double gwa = 90.67; // Stores the user's GWA as a double because it contains decimal places //
+
+        System.out.println("Firstname: " + firstName); // Displays the user's first name //
+        System.out.println("Lastname: " + lastName); // Displays the user's last name //
+        System.out.println("Address: " + address); // Displays the user's address //
+        System.out.println("Mobile Number: " + mobileNumber); // Displays the user's mobile number //
+        System.out.println("Age: " + age); // Displays the user's age //
+        System.out.println("GWA: " + gwa); // Displays the user's GWA //
+
+    } // Ends the main method
+} // Ends the Main class
