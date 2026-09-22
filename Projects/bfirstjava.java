91488702d0a270e02a8171822a817dde9cb32261
NeoZxyyy <abalosolivercromwell@gmail.com>
2026-09-22 21:52:36 +0800

    Laboratory Initial Commit

---
+++ Projects/bfirstjava.java
@@ -0,0 +1,7 @@
+public class bfirstjava {
+    public static void main(String[] args) {
+
+        System.out.println("Oliver Cromwell B. Abalos");
+        System.out.println("BSIT 1B");
+    }
+}
---
+++ Projects/bsecondjava.java
@@ -0,0 +1,17 @@
+public class bsecondjava {
+    public static void main(String[] args) {
+
+        // Variable
+        String name = "Oliver Cromwell B. Abalos";
+        String address = "Laoac, Pangasinan";
+        String contactNum = "09297923375";
+        int age = 20;
+        String crush = "Wala";
+
+        System.out.println("Full Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Contact Number:" + contactNum);
+        System.out.println("Age: " + age);
+        System.out.println("Crush: " + crush);
+    }
+}
